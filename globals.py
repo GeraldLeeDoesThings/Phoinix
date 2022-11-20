ed42794e20d1600783a87511da2bae1e2d9e7ebb
GeraldLeeDoesThings <cheeseyboi@enginity.com>
2022-11-20 18:39:31 -0500

    Fix circular import again again

--- globals.py
+++ globals.py
@@ -1,9 +1,12 @@
-import ba_recruiting
+from __future__ import annotations
 from typing import *
+
+if TYPE_CHECKING:
+    from ba_recruiting import BARun
 
 # Maps User ID (Discord) -> (Token, Character ID (FFXIV))
 verification_map = {}  # type: Dict[int, Dict[str, Union[bool, int, str]]]
 
-ba_run_post_map = {}  # type: Dict[int, ba_recruiting.BARun]
+ba_run_post_map = {}  # type: Dict[int, BARun]
 
 background_tasks = set()
